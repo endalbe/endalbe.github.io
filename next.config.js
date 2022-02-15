51719db924c9c569b45d9edaa65199c583c7365c
Evgeny Agapov <evgenyagap@icloud.com>
2022-02-15 22:03:45 +0300

    edited next config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const path = require('path');
 const debug = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-	assetPrefix: !debug ? 'https://endalbe.github.io' : '',
+	assetPrefix: !debug
+		? 'https://endalbe.github.io'
+		: 'https://endalbe.github.io',
 	reactStrictMode: true,
 	sassOptions: {
 		includePaths: [path.join(__dirname, 'styles')]
