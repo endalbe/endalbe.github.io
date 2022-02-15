dad7f2ba378c6859ea17903c3606a42ea1415685
Evgeny Agapov <evgenyagap@icloud.com>
2022-02-15 19:57:03 +0300

    nojekyll

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
+	assetPrefix: isProd ? '/endalbe/' : '',
 	reactStrictMode: true,
 	sassOptions: {
 		includePaths: [path.join(__dirname, 'styles')]
 	},
 	eslint: {
-		// Warning: This allows production builds to successfully complete even if
-		// your project has ESLint errors.
 		ignoreDuringBuilds: true
 	}
 };
