348c2bc6fc9a8b485fee4580e51f11342beb32b4
endalbe <endalbe@users.noreply.github.com>
2022-02-15 18:14:38 +0000

    deploy: 81d6197d0d2e71bf6e34dd4285fdd04c4cba0116

---
+++ api/repos.js
@@ -0,0 +1,25 @@
+import { Octokit } from '@octokit/core';
+
+export default async function handler(req, res) {
+	const octokit = new Octokit({
+		auth: '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'
+	});
+
+	try {
+		const { data } = await octokit.request('GET /users/{username}/repos', {
+			username: 'endalbe',
+			visibility: 'public'
+		});
+
+		const response = await data;
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.setHeader('Cache-Control', 'max-age=180000');
+		res.end(JSON.stringify(response));
+
+		return res;
+	} catch (error) {
+		res.json(error);
+		res.status(405).end();
+	}
+}
